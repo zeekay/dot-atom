a73f996d26b34d6e46a884d2b25650f3f941b223
Zach Kelling <zk@monoid.io>
2014-04-20 16:02:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# atom
+Just a bunch of dotfiles.
+
+## Install
+Clone and symlink or install with [ellipsis][ellipsis]:
+
+```
+$ ellipsis install atom
+```
+
+[ellipsis]: http://ellipsis.sh
---
+++ ellipsis.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# atom ellipsis module
+
+# The following hooks can be defined to customize behavior of your module:
+# mod.install() {
+#     ellipsis.link_files $(pwd)
+# }
+
+# mod.push() {
+#     git.push
+# }
+
+# mod.pull() {
+#     git.pull
+# }
+
+# mod.status() {
+#     git.status
+# }
